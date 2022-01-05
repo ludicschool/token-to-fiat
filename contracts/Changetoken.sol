261672a595d2b2f77a698d727d8c2a007801189e
Wilson <wilfyan@hotmail.com>
2022-01-04 23:23:21 -0500

    cambios en el contrato

--- contracts/Changetoken.sol
+++ contracts/Changetoken.sol
@@ -9,30 +9,29 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Changetoken is Ownable {
     mapping(uint256 => uint256) public price;
     mapping(uint256 => uint256) public dogma;
-     // aqui poner la direccion    
-    IERC20 public token = IERC20(0xA1dCB17E2C8aCcdce0023Acbd89793124e73142A);
-    event Buy(
-        address owner,
-        uint256 dogma,
-        uint256 price
-    );
+    // aqui poner la direccion
+    IERC20 public token = IERC20(0x8E4899FC4D98cA4bDF5AC3CF04824cFF519A63Cc);
+    event Buy(address owner, uint256 tokenChange, uint256 price);
+
     function buy(
         uint256 _price,
-        uint256 _dogma,
+        uint256 _tokenChange,
         address _address
     ) public onlyOwner returns (bool) {
-        // faltan validaciones de que la cantidad de dogmas sean mayor a 0, que si haya esa cantidad de dogmas en el contrato etc etc 
-        token.transfer(_address, _dogma);        
-        emit Buy(
-            _address,
-            _dogma,
-            _price
+        require(
+            token.balanceOf(address(this)) > 0,
+            "El balance del token del contrato es igual a 0"
         );
+        require(
+            _tokenChange < token.balanceOf(address(this)),
+            "No existe el balance del token en el contrato"
+        );
+        token.transfer(_address, _tokenChange);
+        emit Buy(_address, _tokenChange, _price);
         return true;
     }
-    function withdraw( address _address, uint256 _dogma) public onlyOwner {
-        token.transfer(_address, _dogma);
+
+    function withdraw(address _address, uint256 _tokenChange) public onlyOwner {
+        token.transfer(_address, _tokenChange);
     }
-
-
 }
