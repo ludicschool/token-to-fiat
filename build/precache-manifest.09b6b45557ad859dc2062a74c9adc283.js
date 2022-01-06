f61ef01d15f1b5470aa15afc43ad4f64ff10fd9b
Wilson <wilfyan@hotmail.com>
2022-01-05 21:10:03 -0500

    cambios para subida a produccion

---
+++ build/precache-manifest.09b6b45557ad859dc2062a74c9adc283.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3737ea27826e12a2975b035ed5a3fa7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "218259f7a69fe98f3f58",
+    "url": "/static/css/2.02139679.chunk.css"
+  },
+  {
+    "revision": "ecba12ad2a414deceec2",
+    "url": "/static/css/main.dd4f0c22.chunk.css"
+  },
+  {
+    "revision": "218259f7a69fe98f3f58",
+    "url": "/static/js/2.3019f531.chunk.js"
+  },
+  {
+    "revision": "8ec9d34289b7acdb9c8402afdac4f551",
+    "url": "/static/js/2.3019f531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecba12ad2a414deceec2",
+    "url": "/static/js/main.db65d2c0.chunk.js"
+  },
+  {
+    "revision": "7351034a49e12a151870",
+    "url": "/static/js/runtime-main.b8e82be3.js"
+  }
+]);
